be53ebf3bd4494fdb654519ab23cf1c231e97594
Steve Flinchbaugh <stflinch1@gmail.com>
2016-01-13 11:33:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello GitHub this is Steves 2nd test');
+  res.send('Hello GitHub this is Steves test from GitHub.com');
 });
  
 app.listen(process.env.PORT || 80);
