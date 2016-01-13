faf7601e811e96aadbfeffccfe792d95ec2f4352
EC2 Default User <ec2-user@ip-10-0-0-180.ec2.internal>
2016-01-13 16:23:05 +0000

    change display text in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello GitHub this is still Steve!!!!!!');
+  res.send('Hello GitHub this is Steves test');
 });
  
 app.listen(process.env.PORT || 80);
