eec47a7af2a9da1ad332306c86ee16395cf39fe2
sooyoncho <sooyon.cho@uscis.dhs.gov>
2016-01-14 12:33:36 -0500

    add message

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-<<<<<<< HEAD
+
   res.send('Hello GitHub this is team Redskins');
-=======
-  res.send('Hello GitHub this is Rob');
->>>>>>> refs/remotes/origin/master
+
 });
  
 app.listen(process.env.PORT || 80);
