55a74357559c846f978287f47544614031fef0e2
sooyoncho <sooyon.cho@uscis.dhs.gov>
2016-01-14 12:36:13 -0500

    change message

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ var app = express();
 
 app.get('/', function (req, res) {
 
-<<<<<<< HEAD
   res.send('Hello GitHub this is team Redskins');
-=======
+
   res.send('Hello GitHub this is team name Burroughs');
 
   res.send('Hello GitHub this is Rob');
->>>>>>> refs/remotes/origin/master
 
 });
  
