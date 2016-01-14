d12e3b7c2de0307b43087d786a3797c5be139f29
Robert Cole <robert.m.cole@uscis.dhs.gov>
2016-01-14 12:12:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello GitHub this is rob');
+  res.send('Hello GitHub');
 });
  
 app.listen(process.env.PORT || 80);
