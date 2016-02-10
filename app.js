150676cccbbbf83b24cd077ba347a69eefab85cc
reedflinch <stflinch1@gmail.com>
2016-02-10 14:34:11 +0000

    clean up app.js

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var app = express();
 
 app.get('/', function (req, res) {
 
-  res.send('Hello GitHub this is lisa-thuy-team \n\
-  	Hello GitHub this is team name \n\
-  	Hello GitHub this is team name second time \n\
-  	Hello GitHub this is team name Burroughs \n\
-  	Hello GitHub this is Rob \n\
-  	Hello GitHub this is Venkat Sreekanth');
+  res.send('Hello GitHub this is lisa-thuy-team. \n\
+  	Hello GitHub this is team name. \n\
+  	Hello GitHub this is team Burroughs. \n\
+  	Hello GitHub this is Rob. \n\
+  	Hello GitHub this is Venkat Sreekanth.');
 
 });
 
